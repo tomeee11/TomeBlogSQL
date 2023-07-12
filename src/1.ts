1ee3a687190009bc5a9f5f33012e5c630217c1df
Lee seongwon <jsw6357@naver.com>
2023-07-12 12:37:43 +0900

    1
    1

---
+++ src/1.ts
@@ -0,0 +1,6 @@
+import { f1 } from "./2"
+
+const a = { name: 1};
+const b = { name: 1};
+const c = { name: 1};
+f1
---
+++ src/2.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.f1 = void 0;
+function f1() { }
+exports.f1 = f1;
